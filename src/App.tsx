03d0f2b225cd2d7387cd8c1c5555ef681450b762
amalarajst@gmail.com <respetealmotociclista@gmail.com>
2026-05-02 09:47:58 -0300

    fix: remove unused useState causing tsc build error

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 // Trigger fresh deploy
 import { HashRouter, Routes, Route, useLocation } from 'react-router-dom';
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import { Layout } from './components/layout/Layout';
 import { ToastProvider } from './components/ui/Toast';
 // Pages will be imported here as they are created
