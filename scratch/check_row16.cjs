6f1ea29280903b62b880e6c13a7a213f30b06903
amalarajst@gmail.com <respetealmotociclista@gmail.com>
2026-05-16 23:29:01 -0300

    chore: ignorar carpeta scratch y aligerar repo

--- scratch/check_row16.cjs
+++
@@ -1,9 +0,0 @@
-const XLSX = require('xlsx');
-const path = require('path');
-
-const EXCEL_FILE = path.join('Excel y fotos', 'Preguntas Sep 2023 - ultima versión (1).xlsx');
-const wb = XLSX.readFile(EXCEL_FILE);
-const ws = wb.Sheets[wb.SheetNames[0]];
-const rawData = XLSX.utils.sheet_to_json(ws, { header: 1, defval: '' });
-
-console.log('Row 16:', JSON.stringify(rawData[16]));
